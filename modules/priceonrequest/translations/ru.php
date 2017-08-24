dff8204c9c5a61076e5205765eebeebcd31c3cc3
Dima <gegenya@progroup.by>
2017-08-24 09:17:46 +0300

    Convert translate file to UTF8

--- modules/priceonrequest/translations/ru.php
+++ modules/priceonrequest/translations/ru.php
@@ -2,19 +2,19 @@
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_d4df99494e1bc16b3d1b8fb4bcc27056'] = 'Запрос цены товара';
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Вы уверены что хотите удалить этот модуль?';
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_b5411ca5518c2764291be8f10d1ac8c7'] = 'Введите email  на который будут приходить запросы цены товара.';
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_bcc254b55c4a1babdf1dcb82c207506b'] = 'Телефон';
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_f7a42fe7211f98ac7a60a285ac3a9e87'] = 'телефон';
-$_MODULE['<{priceonrequest}prestashop>priceonrequest_c9cc8cce247e49bae79f15173ce97354'] = 'Сохранить';
-$_MODULE['<{priceonrequest}prestashop>buttonpriceonrequest_df0eed9cf2188204a5202a3bc0e10f43'] = 'Запросить цену';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_6559792f5cfe311e8e23a3ff1b31e035'] = 'Запрос цены на ';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_e9655d559c16112302def677a1e435bb'] = 'Ваше имя *';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_11a433ab6a81fd7c9227e481e16a961a'] = 'Ваш email *';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_3260d29dc6595e71cd4e04afa556f832'] = 'Ваш телефон';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_ef6703d6c606526cece4a9eda9d49045'] = 'Комментарий';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_94966d90747b97d1f0f206c98a8b1ac3'] = 'Отправить';
-$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_4b6180fe5e4a3943f5ce2a939ebe337c'] = 'Ваз запрос отправлен!';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_d4df99494e1bc16b3d1b8fb4bcc27056'] = 'Р—Р°РїСЂРѕСЃ С†РµРЅС‹ С‚РѕРІР°СЂР°';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_bb8956c67b82c7444a80c6b2433dd8b4'] = 'Р’С‹ СѓРІРµСЂРµРЅС‹ С‡С‚Рѕ С…РѕС‚РёС‚Рµ СѓРґР°Р»РёС‚СЊ СЌС‚РѕС‚ РјРѕРґСѓР»СЊ?';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_b5411ca5518c2764291be8f10d1ac8c7'] = 'Р’РІРµРґРёС‚Рµ email  РЅР° РєРѕС‚РѕСЂС‹Р№ Р±СѓРґСѓС‚ РїСЂРёС…РѕРґРёС‚СЊ Р·Р°РїСЂРѕСЃС‹ С†РµРЅС‹ С‚РѕРІР°СЂР°.';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_bcc254b55c4a1babdf1dcb82c207506b'] = 'РўРµР»РµС„РѕРЅ';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_f7a42fe7211f98ac7a60a285ac3a9e87'] = 'С‚РµР»РµС„РѕРЅ';
+$_MODULE['<{priceonrequest}prestashop>priceonrequest_c9cc8cce247e49bae79f15173ce97354'] = 'РЎРѕС…СЂР°РЅРёС‚СЊ';
+$_MODULE['<{priceonrequest}prestashop>buttonpriceonrequest_df0eed9cf2188204a5202a3bc0e10f43'] = 'Р—Р°РїСЂРѕСЃРёС‚СЊ С†РµРЅСѓ';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_6559792f5cfe311e8e23a3ff1b31e035'] = 'Р—Р°РїСЂРѕСЃ С†РµРЅС‹ РЅР° ';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_e9655d559c16112302def677a1e435bb'] = 'Р’Р°С€Рµ РёРјСЏ *';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_11a433ab6a81fd7c9227e481e16a961a'] = 'Р’Р°С€ email *';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_3260d29dc6595e71cd4e04afa556f832'] = 'Р’Р°С€ С‚РµР»РµС„РѕРЅ';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_ef6703d6c606526cece4a9eda9d49045'] = 'РљРѕРјРјРµРЅС‚Р°СЂРёР№';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_94966d90747b97d1f0f206c98a8b1ac3'] = 'РћС‚РїСЂР°РІРёС‚СЊ';
+$_MODULE['<{priceonrequest}prestashop>formpriceonrequest_4b6180fe5e4a3943f5ce2a939ebe337c'] = 'Р’Р°Р· Р·Р°РїСЂРѕСЃ РѕС‚РїСЂР°РІР»РµРЅ!';
 
 return $_MODULE;
